12226c09c63fa2916a6a8fd77993247bd1d0ae06
David Perelroizen <david.perelroizen@gmail.com>
2022-04-25 10:24:13 +0200

    Created the user model

--- softdesk/authentication/models.py
+++ softdesk/authentication/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    first_name = models.CharField()
+    last_name = models.CharField()
+    email = models.EmailField()
+
+    FIRST_NAME_FIELD = 'first_name'
+    LAST_NAME_FIELD = 'last_name'
+    EMAIL_FIELD = 'email'
+
