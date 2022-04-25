e0c414bc185ecbfa7779eae8f3ccd5424ca2b467
David Perelroizen <david.perelroizen@gmail.com>
2022-04-25 11:02:22 +0200

    User model completion and migration

--- softdesk/authentication/models.py
+++ softdesk/authentication/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    first_name = models.CharField()
-    last_name = models.CharField()
-    email = models.EmailField()
+    first_name = models.CharField(max_length=20, unique=False)
+    last_name = models.CharField(max_length=40, unique=False)
+    email = models.EmailField(max_length=40, unique=True)
 
     FIRST_NAME_FIELD = 'first_name'
     LAST_NAME_FIELD = 'last_name'
--- softdesk/softdesk/settings.py
+++ softdesk/softdesk/settings.py
@@ -126,8 +126,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'authentication.User'
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
-    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
 }
