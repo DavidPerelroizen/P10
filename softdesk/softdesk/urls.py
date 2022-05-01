0fb32114b7de6e6440916f4464cd972a55a97202
David Perelroizen <david.perelroizen@gmail.com>
2022-05-01 22:22:47 +0200

    Modified the urls names

--- softdesk/softdesk/urls.py
+++ softdesk/softdesk/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/users/', UserCreate.as_view(), name='user-create')
+    path('api/users/', UserCreate.as_view(), name='signup')
 ]
