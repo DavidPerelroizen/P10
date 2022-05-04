ab9ce2a70fa24a0eccaf29ffc27b7ca89d7010ba
David Perelroizen <david.perelroizen@gmail.com>
2022-05-04 22:27:34 +0200

    Added the router viewset for projects

--- softdesk/softdeskconf/urls.py
+++ softdesk/softdeskconf/urls.py
@@ -18,14 +18,17 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from authentication.views import UserCreate
 from rest_framework import routers
+from softdesk.softdeskapp.views import ProjectsViewset
 
 router = routers.SimpleRouter()
 
+router.register('projects', ProjectsViewset, basename='projects')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/users/', UserCreate.as_view(), name='signup')
+    path('api/users/', UserCreate.as_view(), name='signup'),
+    path('api/', include(router.urls))
 ]
