acb15c8ca2759cad98a0ee07a4dbe58d01cf13a2
David Perelroizen <david.perelroizen@gmail.com>
2022-05-04 22:08:44 +0200

    Added a router

--- softdesk/softdeskconf/urls.py
+++ softdesk/softdeskconf/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from authentication.views import UserCreate
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
