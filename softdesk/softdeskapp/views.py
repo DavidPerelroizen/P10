4fdf89d02b926922b8614e85685ffe1d75c5b5d5
David Perelroizen <david.perelroizen@gmail.com>
2022-05-04 22:27:11 +0200

    Created a Modelviewset for Projects

--- softdesk/softdeskapp/views.py
+++ softdesk/softdeskapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import ProjectSerializer
+from .models import Projects
 
 # Create your views here.
+
+
+class ProjectsViewset(ModelViewSet):
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        return Projects.objects.all()
