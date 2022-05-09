74f7f21a088f116f65e01f419ca6a779072efd88
David Perelroizen <david.perelroizen@gmail.com>
2022-05-09 18:36:11 +0200

    Added routers for Contributors, Issues and comments models

--- softdesk/softdeskapp/urls.py
+++ softdesk/softdeskapp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
-from .views import ProjectsViewset
+from .views import ProjectsViewset, IssuesViewset, CommentsViewset, ContributorsViewset
 from rest_framework import routers
 
 
 router = routers.SimpleRouter()
 
 router.register('projects', ProjectsViewset, basename='projects')
+router.register('issues', IssuesViewset, basename='issues')
+router.register('comments', CommentsViewset, basename='comments')
+router.register('contributors', ContributorsViewset, basename='contributors')
 
 urlpatterns = [
     path('', include(router.urls)),
