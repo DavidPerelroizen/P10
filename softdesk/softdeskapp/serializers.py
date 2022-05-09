675c91fd0a73419e6b57e9a550f20b573ab53b0f
David Perelroizen <david.perelroizen@gmail.com>
2022-05-09 14:59:53 +0200

    Added serializers for Contributors, Issues and comments models

--- softdesk/softdeskapp/serializers.py
+++ softdesk/softdeskapp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Projects
+from .models import Projects, Contributors, Issues, Comments
 
 
 class ProjectSerializer(ModelSerializer):
@@ -7,3 +7,22 @@ class ProjectSerializer(ModelSerializer):
     class Meta:
         model = Projects
         fields = ['title', 'description', 'type', 'author_user_id']
+
+
+class ContributorSerializer(ModelSerializer):
+
+    class Meta:
+        model = Contributors
+        fields = ['user_id', 'project_id', 'permission', 'role']
+
+
+class IssueSerializer(ModelSerializer):
+    class Meta:
+        model = Issues
+        fields = ['title', 'desc', 'tag', 'priority', 'project_id', 'status', 'author_user_id', 'assignee_user_id']
+
+
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comments
+        fields = ['description', 'author_user_id', 'issue_id']
