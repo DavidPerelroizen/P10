84de0c40b29e0b3ce3f9342b25f49bfbb58b2e17
David Perelroizen <david.perelroizen@gmail.com>
2022-05-11 18:40:38 +0200

    Reorganized the serializers from children to parents

--- softdesk/softdeskapp/serializers.py
+++ softdesk/softdeskapp/serializers.py
@@ -2,6 +2,18 @@ from rest_framework.serializers import ModelSerializer
 from .models import Projects, Contributors, Issues, Comments
 
 
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comments
+        fields = ['description', 'author_user_id', 'issue_id']
+
+
+class IssueSerializer(ModelSerializer):
+    class Meta:
+        model = Issues
+        fields = ['title', 'desc', 'tag', 'priority', 'project_id', 'status', 'author_user_id', 'assignee_user_id']
+
+
 class ContributorSerializer(ModelSerializer):
 
     class Meta:
@@ -16,14 +28,3 @@ class ProjectSerializer(ModelSerializer):
         model = Projects
         fields = ['title', 'description', 'type', 'author_user_id', 'contributors']
 
-
-class IssueSerializer(ModelSerializer):
-    class Meta:
-        model = Issues
-        fields = ['title', 'desc', 'tag', 'priority', 'project_id', 'status', 'author_user_id', 'assignee_user_id']
-
-
-class CommentSerializer(ModelSerializer):
-    class Meta:
-        model = Comments
-        fields = ['description', 'author_user_id', 'issue_id']
