230aa22c9b205b99dc8d1b237ad6d0cb375da231
David Perelroizen <david.perelroizen@gmail.com>
2022-05-16 14:41:53 +0200

    Added response status to each method

--- softdesk/softdeskapp/views.py
+++ softdesk/softdeskapp/views.py
@@ -41,10 +41,10 @@ class ContributorsAPIView(APIView):
             contributor.save()
             data = {'user_id': contributor.user_id.id, 'project_id': contributor.project_id.id,
                     'permission': contributor.permission, 'role': contributor.role}
-            return Response(data)
+            return Response(data, status=status.HTTP_201_CREATED)
         except Exception as e:
             print(e)
-            return Response({'Contributor posting failed'})
+            return Response({'Contributor posting failed'}, status=status.HTTP_404_NOT_FOUND)
 
 
 class ContributorDeletion(APIView):
@@ -55,7 +55,7 @@ class ContributorDeletion(APIView):
             return Response({'message': f'Contributor {user_id} deleted'})
         except Exception as e:
             print(e)
-            return Response({'message': 'Contributor not found'})
+            return Response({'message': 'Contributor not found'}, status=status.HTTP_404_NOT_FOUND)
 
 
 class IssuesAPIView(APIView):
@@ -81,23 +81,23 @@ class IssuesAPIView(APIView):
                     'project_id': issue.project_id.id, 'status': issue.status,
                     'author_user_id': issue.author_user_id.id,
                     'assignee_user_id': issue.assignee_user_id.id, 'created_time': issue.created_time}
-            return Response(data)
+            return Response(data, status=status.HTTP_201_CREATED)
         except Exception as e:
             print(e)
-            return Response({'Issue posting failed'})
+            return Response({'Issue posting failed'}, status=status.HTTP_404_NOT_FOUND)
 
 
 class IssuesModifyAPIView(APIView):
     def get(self, request, pk, issue_id):
         issue_to_get = Issues.objects.filter(Q(project_id=pk) & Q(id=issue_id))
         serializer = IssueSerializer(issue_to_get, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, pk, issue_id):
         try:
             issue_to_delete = Issues.objects.filter(Q(project_id=pk) & Q(id=issue_id))
             issue_to_delete.delete()
-            return Response({'message': f'Issue {issue_id} deleted'})
+            return Response({'message': f'Issue {issue_id} deleted'}, status=status.HTTP_200_OK)
         except Exception as e:
             print(e)
             return Response({'message': 'Issue not found'})
@@ -115,10 +115,10 @@ class IssuesModifyAPIView(APIView):
             issue_to_update.author_user_id = get_object_or_404(User, id=int(request.data['author_user_id']))
             issue_to_update.assignee_user_id = get_object_or_404(User, id=int(request.data['assignee_user_id']))
             issue_to_update.save()
-            return Response({'message': f'Issue {issue_id} modified'})
+            return Response({'message': f'Issue {issue_id} modified'}, status=status.HTTP_200_OK)
         except Exception as e:
             print(e)
-            return Response({'message': 'Issue not found'})
+            return Response({'message': 'Issue not found'}, status=status.HTTP_404_NOT_FOUND)
 
 
 class CommentsAPIView(APIView):
@@ -127,7 +127,7 @@ class CommentsAPIView(APIView):
         issue_to_get = issue[0]
         comments = Comments.objects.filter(issue_id=issue_to_get.id)
         serializer = CommentSerializer(comments, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, pk, issue_id):
         try:
@@ -140,10 +140,10 @@ class CommentsAPIView(APIView):
             comment.save()
             data = {'description': comment.description, 'issue_id': comment.issue_id.id,
                     'author_user_id': comment.author_user_id.id}
-            return Response(data)
+            return Response(data, status=status.HTTP_201_CREATED)
         except Exception as e:
             print(e)
-            return Response({'Comment posting failed'})
+            return Response({'Comment posting failed'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CommentsModifyAPIView(APIView):
@@ -152,17 +152,17 @@ class CommentsModifyAPIView(APIView):
         issue_to_get = issue[0]
         comment = Comments.objects.filter(Q(issue_id=issue_to_get.id) & Q(id=comment_id))
         serializer = CommentSerializer(comment, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, pk, issue_id, comment_id):
         try:
             issue = get_object_or_404(Issues, Q(project_id=pk) & Q(id=issue_id))
             comment_to_delete = get_object_or_404(Comments, Q(id=comment_id) & Q(issue_id=issue.id))
             comment_to_delete.delete()
-            return Response({'message': f'Issue {comment_id} deleted'})
+            return Response({'message': f'Issue {comment_id} deleted'}, status=status.HTTP_200_OK)
         except Exception as e:
             print(e)
-            return Response({'message': 'Comment not found'})
+            return Response({'message': 'Comment not found'}, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk, issue_id, comment_id):
         try:
@@ -173,8 +173,8 @@ class CommentsModifyAPIView(APIView):
             comment_to_update.author_user_id = get_object_or_404(User, id=int(request.data['author_user_id']))
             comment_to_update.issue_id = issue_to_get
             comment_to_update.save()
-            return Response({'message': f'Issue {comment_id} modified'})
+            return Response({'message': f'Issue {comment_id} modified'}, status=status.HTTP_200_OK)
         except Exception as e:
             print(e)
-            return Response({'message': 'Comment not found'})
+            return Response({'message': 'Comment not found'}, status=status.HTTP_404_NOT_FOUND)
 
