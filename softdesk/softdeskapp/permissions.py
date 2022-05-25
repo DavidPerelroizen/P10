92bb76d032b9b6b61609e746e30f8cc45d33a35b
David Perelroizen <david.perelroizen@gmail.com>
2022-05-25 07:59:25 +0200

    Deleted the CanManageContributors permission as it wasn't adapted. Removed the method has_permission from IsProjectCreator and IsProjectContributor. Added a is_authenticated condition to the two latter classes

--- softdesk/softdeskapp/permissions.py
+++ softdesk/softdeskapp/permissions.py
@@ -5,25 +5,29 @@ from django.shortcuts import get_object_or_404
 
 class IsProjectCreator(BasePermission):
 
-    def has_permission(self, request, view):
-        return bool(request.user.is_authenticated)
-
     def has_object_permission(self, request, view, obj):
-        requester = request.user
-        if requester.id == obj.author_user_id.id:
-            return True
+        if request.user.is_authenticated:
+            requester = request.user
+            if requester.id == obj.author_user_id.id:
+                print(requester.id)
+                print(obj.author_user_id.id)
+                return True
+            else:
+                return False
+        else:
+            return False
 
 
 class IsProjectContributor(BasePermission):
 
-    def has_permission(self, request, view):
-        return bool(request.user.is_authenticated)
-
     def has_object_permission(self, request, view, obj):
-        requester = request.user
-        contributors_list = Contributors.objects.filter(project_id=obj.id)
-        if requester in contributors_list and request.method in SAFE_METHODS:
-            return True
+        if request.user.is_authenticated:
+            requester = request.user
+            contributors_list = Contributors.objects.filter(project_id=obj.id)
+            if requester in contributors_list and request.method in SAFE_METHODS:
+                return True
+        else:
+            return False
 
 
 class CanAccessCreateCommentIssue(BasePermission):
@@ -58,15 +62,4 @@ class IsCommentOwner(BasePermission):
         requester = request.user
         issue_author_id = obj.author_user_id
         if requester.id == issue_author_id.id:
-            return True
-
-
-class CanManageContributors(BasePermission):
-    def has_permission(self, request, view):
-        return bool(request.user.is_authenticated)
-
-    def has_object_permission(self, request, view, obj):
-        requester = request.user
-        project_impacted = get_object_or_404(Projects, id=obj.project_id)
-        if requester.id == project_impacted.author_user_id.id:
             return True
